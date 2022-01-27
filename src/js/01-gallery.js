fa60ba8512a843d45819d6a1d1bc2e40c62c4685
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-27 20:36:22 +0200

    firstDobeVithBug

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,20 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join('');    
+}
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
