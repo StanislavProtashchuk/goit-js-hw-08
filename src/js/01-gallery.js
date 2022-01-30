35afd65005b8cf104c6dd637b37725462009c9ae
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-01-30 14:00:39 +0200

    templates

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+import galleryTpl from '../templates/gallery.hbs';
 // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
@@ -9,14 +10,8 @@ const cardsMarkup = createGallery(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-
 function createGallery(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
-    }).join('');    
+    return galleryItems.map(galleryTpl).join('');    
 }
-
 
 let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
